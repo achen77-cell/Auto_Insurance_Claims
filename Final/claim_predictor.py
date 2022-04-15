57717e7751c4b88aa86c43aa818d97fdafe0a2d6
aishwaryarao595 <aishwarya.rao595@gmail.com>
2022-04-14 22:36:03 -0400

    code integration

--- Final/claim_predictor.py
+++ Final/claim_predictor.py
@@ -1,5 +1,24 @@
+
 import pandas as pd
 import json
+import requests
+# import ../AutoML/Databricks/Azure
+
+def create_tf_serving_json(data):
+    return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
+
+
+def score_model(model_uri, databricks_token, data):
+    headers = {
+            "Authorization": f"Bearer {databricks_token}",
+            "Content-Type": "application/json",
+  }
+    data_json = data.to_dict(orient='records') if isinstance(data, pd.DataFrame) else create_tf_serving_json(data)
+    response = requests.request(method='POST', headers=headers, url=model_uri, json=data_json)
+    if response.status_code != 200:
+        raise Exception(f"Request failed with status {response.status_code}, {response.text}")
+    return response.json()
+
 
 def get_result(input_json):
     '''
@@ -50,7 +69,7 @@ def get_result(input_json):
 
     input_df = pd.DataFrame(input_json, index=[0])
     try:
-        output = get_model_predictions(input_df)
+        output = score_model(input_df)
         if output == 0:
             result = "Genuine"
         else:
