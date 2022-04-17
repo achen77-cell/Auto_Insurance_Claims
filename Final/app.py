7f8744b8c4c529a5f0bd35021c5766e8385006dc
Nehal <nehaljain.1292@gmail.com>
2022-04-17 19:11:18 -0400

    Change to allow cross origin hosts

--- Final/app.py
+++ Final/app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 from claim_predictor import get_result
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class PredictionRequestFields(BaseModel):
         MonthOfAccident: str
